038751b194ee4be7d7b054ee108ed2cd35edd8b5
AlmirCarlettiNeto <almir.carletti.neto@gmail.com>
2024-09-03 19:04:57 -0300

    implemented put, get and delete http methods

--- src/controller/UserController.js
+++ src/controller/UserController.js
@@ -3,11 +3,67 @@ import { User } from "../models/User.js";
 export async function createUser(req, res) {
   try {
     const { name, email } = req.body;
-    const user = await User.create({ name, email });
-    res.status(200).json({ user });
+
+    let user = await User.findOne({ where: { email } }); // para não permitir que dois usuários sejam cadastrados com o mesmo e-mail
+    if (user) {
+      res.status(200).json({ message: "Já existe um usuário com este e-mail" });
+    } else {
+      user = await User.create({ name, email });
+      res.status(200).json({ user });
+    }
   } catch (error) {
     console.error("Error creating user:", error);
     console.log(error);
     res.status(500).json({ error: "Failed to create user" });
+  }
+}
+
+export async function updateUser(req, res) {
+  try {
+    const { id } = req.params;
+    const { name, email } = req.body;
+
+    let user = await User.findOne({ where: { id } });
+
+    if (!user) {
+      res.status(401).json({ message: "Nenhum usuário encontrado" });
+    } else {
+      user = await User.update({ name, email }, { where: { id } });
+      res.status(200).json({ user });
+    }
+  } catch (error) {
+    console.error("Error updating user:", error);
+    console.log(error);
+    res.status(400).json({ error: "Failed to update user" });
+  }
+}
+
+export async function listUsers(req, res) {
+  try {
+    const users = await User.findAll();
+
+    if (!users) {
+      res.status(200).json({ message: "Não há usuários cadastrados" });
+    } else {
+      res.status(200).json({ users });
+    }
+  } catch (error) {
+    res.status(400).json({ error });
+  }
+}
+
+export async function deleteUser(req, res) {
+  try {
+    const { id } = req.params;
+    let user = await User.findOne({ where: { id } });
+
+    if (!user) {
+      res.status(401).json({ message: "Usuário não encontrado" });
+    } else {
+      await User.destroy({ where: { id } });
+      res.status(200).json({ message: `Usuário com o id ${id} apagado` });
+    }
+  } catch (error) {
+    res.status(500).json({ error: "Erro ao apagar o usuário" });
   }
 }
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,17 @@
 import { Router } from "express";
-import { createUser } from './controller/UserController.js';
+import {
+  createUser,
+  deleteUser,
+  listUsers,
+  updateUser,
+} from "./controller/UserController.js";
 
 const router = Router();
 
 // método para criar um novo usuário
 router.post("/create-user", createUser);
+router.put("/update-user/:id", updateUser); // route paramater passado. Assim, id será desestruturado de req.params
+router.get("/list-user", listUsers);
+router.delete("/delete-user/:id", deleteUser);
 
 export default router;
